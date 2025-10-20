88a176b68db7f9cf44376cb44de6e7c0ab9c2f4f
tanulo <tanulo@barossgabor.local>
2025-10-20 14:40:42 +0200

    mouseover event

---
+++ 5_mouseover_event/script.js
@@ -0,0 +1,27 @@
+const demo = document.querySelector('#demo')
+// console.log(demo)
+
+let isImageDisplayed = false;
+
+demo.addEventListener('mouseover', (event) => {
+    //console.log(event)
+
+    if (!isImageDisplayed && event.target===demo) {
+        const img = document.createElement('img')
+        img.src = './img/kep.jpg';
+        img.alt = 'kep';
+
+        demo.replaceChilder(img);
+        isImageDisplayed = true;
+
+
+    }
+});
+
+demo.addEventListener('mouseout', (event) => {
+    if (isImageDisplayed && event.target===demo) {
+        const p = document.createElement('p')
+
+        demo.replaceChild
+    }
+})
