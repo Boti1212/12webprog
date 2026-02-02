3b6a906e6601d1fd679244dc6fc0fd18218b149a
tanulo <tanulo@barossgabor.local>
2026-02-02 13:47:46 +0100

    10 kutya grid

---
+++ 10_kutya_grid/script.js
@@ -0,0 +1,64 @@
+/*
+<div class="card">
+                <img src="./img/dog-1.jpg" alt="dog3">
+                <div class="content">
+                    <h3>Kártya #1</h3>
+                    <p>dog-1.jpg</p>
+                </div>
+</div>
+*/
+
+const cards = document.querySelector('#cards')
+
+const LISTA_URL = 'src/kepek.txt'
+const IMG_MAPPA = 'img/'
+
+window.addEventListener('DOMContentLoaded', loadCards)
+
+async function loadCards() {
+    try {
+        const res = await fetch(LISTA_URL)
+       
+        if (!res.ok){
+            return alert("Fetch error")
+        }
+
+        const text = await res.text()
+        const files = text.split('\n').map(sor => sor.trim()).filter(sor => sor.length > 0)
+       
+        if (files.length === 0) {
+            return alert('Empty list')
+        }
+
+        cards.innerHTML = ""
+        files.forEach((fileName, index) => {
+            cards.appendChild(createCard(fileName, index))
+        })
+    } catch (error) {
+        alert(`Hiba: ${error}`)
+    }
+}
+
+function createCard(fileName, index) {
+    const card = document.createElement('div')
+    card.className = 'card'
+
+    const image = document.createElement('img')
+    image.src = `${IMG_MAPPA}${fileName}`
+    image.alt = `${fileName}`
+    card.append(image)
+
+    const content = document.createElement('div')
+    content.classList.add('content')
+
+    const h31 = document.createElement('h3')
+    h31.textContent = `Kártya#${index+1}`
+    content.append(h31)
+
+    const p = document.createElement('p')
+    p.textContent = `${fileName}`
+    content.append(p)
+
+    card.append(content)
+    return card
+}
